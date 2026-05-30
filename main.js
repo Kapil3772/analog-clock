841c02ada474a60c41bf1dc4ac43cbb533bba362
Kapil3772 <k.asid721011@gmail.com>
2026-05-30 15:43:14 +0545

    Added theme Functionality

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
+const Angle = {
+    30 : Math.PI/6,
+    10 : Math.PI/18
+}
+
+function loadImage(src) {
+    const img = new Image();
+    img.src = src;
+    return img;
+}
+
+class CustomImageData {
+    constructor(img,widthFactor=1,heightFactor=1,xOffset=0,yOffset=0,flip=false){
+        this.img =img;
+        this.widthFactor = widthFactor;
+        this.heightFactor = heightFactor;
+        this.xOffset = xOffset;
+        this.yOffset = yOffset;
+        this.flip = flip;
+    }
+}
 class Theme {
-    constructor(borderColor, bodyColor, labelColor, handsColor, secondHandColor, innerShadowColor, labelFont){
+    constructor(borderColor, bodyColor, handsColor, secondHandColor, innerShadowColor, labelFont="600 28px Georgia", labelColor="white"){
         this.borderColor = borderColor;
         this.bodyColor = bodyColor;
         this.labelColor = labelColor;
@@ -10,39 +31,80 @@ class Theme {
     }
 }
 
+const Themes = {
+    SubmarinerDate16610SuperLuminova : new Theme(null,null,null,null,null)
+};
+class Style {
+    constructor(caseRad,bezelRad,transitionRingRad,dialFaceRad){
+        this.caseRad = caseRad;
+        this.bazelRad = bezelRad;
+        this.transitionRingRad = transitionRingRad;
+        this.dialFaceRad = dialFaceRad;
+    }
+}
+
+const Styles = {
+    SubmarinerDate16610SuperLuminova : new Style(271,null,null,null)
+};
+
+const DesignType = {
+    IMAGE : "IMAGE",
+    CANVAS : "CANVAS",
+}
 const MotionType = {
     CONTINUOUS : "CONTINUOUS",
     DISCRETE : "DISCRETE"
 }
 
-const Themes = {
-    ROLEX_CLASSIC : new Theme(
-        "#C8A951","#111111","#E7D7A2","#F8F4E8","#D4AF37","rgba(0,0,0,0.75)","600 28px Georgia"),
-    ROLEX_GREEN : new Theme(
-        "#C8A951","#0E2B1F","#EADAA8","#FFF8E7","#D4AF37","rgba(0,0,0,0.78)","600 28px Georgia"),
-    OMEGA_SPEEDMASTER : new Theme(
-        "#A8ADB4","#121212","#E8EAED","#F8FAFC","#D63A3A","rgba(0,0,0,0.82)","600 27px Helvetica"),
-    GSHOCK_STEALTH : new Theme(
-        "#3D4148","#101214","#D6D8DB","#F5F5F5","#FF5C5C","rgba(0,0,0,0.88)","bold 26px Impact"),
-    GSHOCK_MILITARY : new Theme(
-        "#55624C","#1B1F18","#D7DCCF","#EEF2E8","#F0B429","rgba(0,0,0,0.85)","bold 26px Impact"),
-    AP_ROYAL_OAK : new Theme(
-        "#9EA7B3","#20242B","#F1F3F5","#FAFAFA","#7DA0C4","rgba(0,0,0,0.78)","600 27px Helvetica"),
-    PATEK_NAVY : new Theme(
-        "#D1B26A","#0B1833","#F2E7C8","#FFF8E8","#D6B15A","rgba(0,0,0,0.80)","600 28px Georgia"),
-    SEIKO_DIVER : new Theme(
-        "#D0D5DB","#0D1B2A","#EAF4FF","#F8FBFF","#3BA7FF","rgba(0,0,0,0.82)","600 27px Helvetica"),
-    TAG_HEUER : new Theme(
-        "#C7CCD3","#15181C","#F4F6F8","#FFFFFF","#E53935","rgba(0,0,0,0.80)","600 27px Helvetica"),
-    RICHARD_MILLE : new Theme(
-        "#858B93","#0B0D10","#E7EAEE","#FAFAFA","#FF7A00","rgba(0,0,0,0.88)","600 28px Segoe UI"),
-    CARTIER_CLASSIC : new Theme(
-        "#C8A45A","#F7F3EB","#3C3C3C","#1C1C1C","#2B5CAA","rgba(0,0,0,0.18)","600 28px Georgia"),
-    PILOT_AVIATOR : new Theme(
-        "#80858C","#0F1113","#F1F3F4","#FFFFFF","#FF9F1C","rgba(0,0,0,0.86)","600 27px Trebuchet MS"),
-    BELL_ROSS_BEIGE : new Theme(
-        "rgba(185,190,195,0.42)","rgba(221,209,181,0.96)","#2A2A2A","#111111","#7C1111","rgba(0,0,0,0.22)","600 27px Trebuchet MS")
+const Designs = {
+    SubmarinerDate16610SuperLuminova: {
+        name: "Submariner Date 16610 SuperLuminova",
+        designType : DesignType.IMAGE,
+        bodyImg : loadImage("assets/body.png"),
+        hourHandData : {
+                img:loadImage("assets/hour.png"),
+                motionType:MotionType.CONTINUOUS,
+                xOffset: -0.1, //meaning the hand is attached to clock at middle
+                yOffset : -0.5,
+                widthFactor:0.11,
+                heightFactor:1,
+                flip : false,
+            },
+        minuteHandData: {
+                img:loadImage("assets/minute.png"),
+                motionType:MotionType.CONTINUOUS,
+                xOffset: -0.08,
+                yOffset : -0.5,
+                widthFactor:0.15,
+                heightFactor:1,
+                flip : false,
+            },
+        secondHandData: {
+                img:loadImage("assets/second.png"),
+                motionType:MotionType.DISCRETE,
+                xOffset: -0.3,
+                yOffset : -0.5,
+                widthFactor:0.21,
+                heightFactor:1,
+                flip : false,
+            },
+        knobData : {
+            img : loadImage("assets/knob.png"),
+            widthFactor: 0.036,
+            xOffset: 2,
+            yOffset : -2,
+        },
+        style : Styles.SubmarinerDate16610SuperLuminova,
+        theme : Themes.SubmarinerDate16610SuperLuminova,
+        
+        customRender(ctx,clock){
+        }
+    }
+
 };
+
+
+
 
 class Hand {
     constructor(x,y,w,h,unitDegreeDisplacement,initialDeg,unitStepTime,color,initAccumulatorTime=0,motionType=MotionType.DISCRETE){
@@ -77,12 +139,40 @@ class Hand {
         }
         
     }
-    render(ctx){
+    render(ctx,imgData){
         ctx.save();
         ctx.translate(this.x,this.y);
         ctx.rotate(this.currentDeg);
-        ctx.fillStyle = this.handColor;
-        ctx.fillRect(-20,-this.h/2,this.w,this.h);
+        if(imgData!=null){
+            ctx.strokeStyle = "red";
+            if(imgData.flip){
+                ctx.drawImage(
+                    imgData.img,
+                    0+imgData.xOffset+(imgData.img.width * imgData.widthFactor),
+                    0+imgData.yOffset,
+                    -imgData.img.width * imgData.widthFactor,
+                    imgData.img.height * imgData.heightFactor
+                );
+            }else{
+                ctx.drawImage(
+                    imgData.img,
+                    0+imgData.xOffset,
+                    0+imgData.yOffset,
+                    imgData.img.width * imgData.widthFactor,
+                    imgData.img.height * imgData.heightFactor
+                );
+            }
+            
+            // ctx.strokeRect(
+            //     0+imgData.xOffset,
+            //     0+imgData.yOffset,
+            //     imgData.img.width * imgData.widthFactor,
+            //     imgData.img.height * imgData.heightFactor
+            // );
+        }else{
+            ctx.fillStyle = this.handColor;
+            ctx.fillRect(0,-this.h/2,this.w,this.h);
+        }
         ctx.restore();
     }
     setHandColour(color){
@@ -109,8 +199,12 @@ class Label {
         // ctx.beginPath();
         // ctx.arc(this.x,this.y,10,0,Math.PI*2);
         // ctx.fill();
-        ctx.fillStyle = this.clock.currentTheme.labelColor;
-        ctx.font = this.clock.currentTheme.labelFont;
+        if(this.clock.currentDesign.theme.labelColor==null){
+            ctx.fillStyle="black";
+        }else{
+            ctx.fillStyle = this.clock.currentDesign.theme.labelColor;
+        }
+        ctx.font = this.clock.currentDesign.theme.labelFont;
         let renderLabel = ((this.label + 2)%this.clock.noLabels) + 1;
         ctx.fillText(renderLabel,this.x-10,this.y+12.5);
     }
@@ -119,14 +213,14 @@ class Label {
 class Clock {
     constructor(radius,hrs,mins,secs,app){
         this.app = app;
-        this.x = this.app.canvasWidth/2;
-        this.y = this.app.canvasHeight/2;
+        this.x = this.app.canvasWidth/2; //centerX
+        this.y = this.app.canvasHeight/2; //centerY
         this.radius = radius;
         this.hrs = hrs;
         this.mins = mins;
         this.secs = secs;
         //Appearance
-        this.currentTheme = Themes.BELL_ROSS_BEIGE;
+        this.currentDesign = this.app.defaultDesign;
         this.screwWidth = this.radius*0.05;
         this.screwHeight = this.screwWidth;
 
@@ -140,13 +234,38 @@ class Clock {
 
         let initialDeg = ((Math.PI/30)*this.secs) - Math.PI/2;
         let accumulatedTime = 0;
-        this.secondHand = new Hand(this.x,this.y,this.radius*0.88,6,(Math.PI/(6*5)),initialDeg,1,this.currentTheme.secondHandColor,accumulatedTime,MotionType.DISCRETE);
+        this.secondHand = new Hand(this.x,this.y,
+            this.radius,
+            6,
+            Angle[30]/5,
+            initialDeg,
+            1,
+            this.currentDesign.theme.secondHandColor,
+            accumulatedTime,
+            this.currentDesign.secondHandData.motionType
+        );
 
         initialDeg = ((Math.PI/30)*this.mins) - Math.PI/2;
-        this.minuteHand = new Hand(this.x,this.y,this.radius*0.88,8,(Math.PI/(30)),initialDeg,60,this.currentTheme.handsColor,this.secs,MotionType.CONTINUOUS);
+        this.minuteHand = new Hand(this.x,this.y,
+            this.radius*0.88,
+            8,
+            (Math.PI/(30)),
+            initialDeg,60,
+            this.currentDesign.theme.handsColor,
+            this.secs,
+            MotionType.CONTINUOUS
+        );
 
         initialDeg = ((Math.PI/6)*this.hrs) - Math.PI/2;
-        this.hourHand = new Hand(this.x,this.y,this.radius*0.5,10,(Math.PI/6),initialDeg,(60*60),this.currentTheme.handsColor,this.mins*60,MotionType.CONTINUOUS);
+        this.hourHand = new Hand(this.x,this.y,
+            this.radius*0.5,
+            10,
+            (Math.PI/6),
+            initialDeg,
+            (60*60),
+            this.currentDesign.theme.handsColor,
+            this.mins*60,MotionType.CONTINUOUS
+        );
     }
     update(dt){
         this.secondHand.update(dt);
@@ -154,49 +273,120 @@ class Clock {
         this.hourHand.update(dt);
     }
     render(ctx){
-        //outer body rendering
-        ctx.fillStyle = this.currentTheme.borderColor;
-        ctx.beginPath();
-        ctx.arc(this.x,this.y,this.radius+18,0,Math.PI*2);
-        ctx.fill();
-        //Shadow rendering
-        ctx.fillStyle = this.currentTheme.innerShadowColor;
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.arc(this.x,this.y,this.radius+7,0,Math.PI*2);
-        ctx.fill();
-        //inner Body
-        ctx.fillStyle = this.currentTheme.bodyColor;
-        ctx.beginPath();
-        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
-        ctx.fill();
+        if(this.currentDesign.designType==DesignType.CANVAS){
+            //outer body rendering
+            ctx.fillStyle = this.currentTheme.borderColor;
+            ctx.beginPath();
+            ctx.arc(this.x,this.y,this.radius+18,0,Math.PI*2);
+            ctx.fill();
+            //Shadow rendering
+            ctx.fillStyle = this.currentTheme.innerShadowColor;
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.arc(this.x,this.y,this.radius+7,0,Math.PI*2);
+            ctx.fill();
+            //inner Body
+            ctx.fillStyle = this.currentTheme.bodyColor;
+            ctx.beginPath();
+            ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
+            ctx.fill();
 
-        //labels rendering
-        this.renderLabels(ctx);
-        this.renderHands(ctx);
-        //Screw rendering
-        ctx.fillStyle=this.currentTheme.borderColor;
-        ctx.fillRect(this.x - this.screwWidth/2,this.y - this.screwHeight/2,this.screwWidth,this.screwHeight);
-        ctx.strokeStyle=this.currentTheme.bodyColor;
-        ctx.strokeRect(this.x - this.screwWidth/2,this.y - this.screwHeight/2,this.screwWidth,this.screwHeight);
+            //labels rendering
+            this.renderLabels(ctx);
+            this.renderHands(ctx,null);
+            //Screw rendering
+            ctx.fillStyle=this.currentTheme.borderColor;
+            ctx.fillRect(this.x - this.screwWidth/2,this.y - this.screwHeight/2,this.screwWidth,this.screwHeight);
+            ctx.strokeStyle=this.currentTheme.bodyColor;
+            ctx.strokeRect(this.x - this.screwWidth/2,this.y - this.screwHeight/2,this.screwWidth,this.screwHeight);
+        }else{
+            const bodyImg = this.currentDesign.bodyImg;
+            let imgW = this.currentDesign.style.caseRad * 2;
+            let imgH = this.currentDesign.style.caseRad * 2;
+            let imgX = (this.app.canvas.width - imgW)/2;
+            let imgY = (this.app.canvas.height - imgH)/2;
+
+            ctx.save();
+            ctx.translate(this.x,this.y);
+            ctx.rotate(Math.PI*2/180);
+            ctx.drawImage(bodyImg,-imgW/2,-imgH/2,imgW,imgH);
+            ctx.restore();
+
+            // ctx.fillStyle = "rgba(0,0,255,0.5)";
+            // ctx.beginPath();
+            // ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
+            // ctx.fill();
+            // ctx.strokeStyle = "blue";
+            // ctx.strokeRect(imgX,imgY,imgW,imgH);
+
+            const secondHandImg = this.currentDesign.secondHandData.img;
+            let sizeFactor = this.currentDesign.secondHandData.widthFactor;
+            imgW = secondHandImg.width*sizeFactor;
+            imgH = secondHandImg.height*sizeFactor;
+            let handXOffset = imgW*this.currentDesign.secondHandData.xOffset;
+            let handYOffset = imgH*this.currentDesign.secondHandData.yOffset;
+
+            this.secondHand.render(ctx,new CustomImageData(
+                secondHandImg,
+                sizeFactor,
+                sizeFactor,
+                handXOffset,
+                handYOffset,
+                this.currentDesign.secondHandData.flip)
+            );
+            const minuteHandImg = this.currentDesign.minuteHandData.img;
+            sizeFactor = this.currentDesign.minuteHandData.widthFactor;
+            imgW = minuteHandImg.width*sizeFactor;
+            imgH = minuteHandImg.height*sizeFactor;
+            handXOffset = imgW*this.currentDesign.minuteHandData.xOffset;
+            handYOffset = imgH*this.currentDesign.minuteHandData.yOffset;
+            this.minuteHand.render(ctx,new CustomImageData(
+                minuteHandImg,
+                sizeFactor,
+                sizeFactor,
+                handXOffset,
+                handYOffset,
+                this.currentDesign.minuteHandData.flip)
+            );
+            const hourHandImg = this.currentDesign.hourHandData.img;
+            sizeFactor = this.currentDesign.hourHandData.widthFactor;
+            imgW = hourHandImg.width*sizeFactor;
+            imgH = hourHandImg.height*sizeFactor;
+            handXOffset = imgW*this.currentDesign.hourHandData.xOffset;
+            handYOffset = imgH*this.currentDesign.hourHandData.yOffset;
+            this.hourHand.render(ctx,new CustomImageData(
+                hourHandImg,
+                sizeFactor,
+                sizeFactor,
+                handXOffset,
+                handYOffset,
+                this.currentDesign.hourHandData.flip)
+            );
+            const knobImg = this.currentDesign.knobData.img;
+            sizeFactor = this.currentDesign.knobData.widthFactor;
+            imgW = knobImg.width*sizeFactor;
+            imgH = knobImg.height*sizeFactor;
+            imgX = (this.x - imgW/2) + this.currentDesign.knobData.xOffset;
+            imgY = (this.x - imgH/2) + this.currentDesign.knobData.yOffset;
+            ctx.drawImage(knobImg,imgX,imgY,imgW,imgH);
+        }
+        
     }
     renderLabels(ctx){
         for(const label of this.labels){
             label.render(ctx);
         }
     }
-    renderHands(ctx){
-        this.secondHand.render(ctx);
-        this.minuteHand.render(ctx);
-        this.hourHand.render(ctx);
-
+    renderHands(ctx,imgData){
+        this.minuteHand.render(ctx,null);
+        this.hourHand.render(ctx,null);
     }
 }
 
 class ClockApp {
     constructor(){
-        this.canvasWidth = 500;
-        this.canvasHeight = 500;
+        this.canvasWidth = 600;
+        this.canvasHeight = 600;
         this.init();
     }
     init(){
@@ -206,7 +396,8 @@ class ClockApp {
         this.canvas.width = this.canvasWidth;
         this.canvas.height = this.canvasHeight;
         const now = new Date();
-        this.clock = new Clock(200,now.getHours(),now.getMinutes(),now.getSeconds(),this);
+        this.defaultDesign = Designs.SubmarinerDate16610SuperLuminova;
+        this.clock = new Clock(this.defaultDesign.style.caseRad,now.getHours(),now.getMinutes(),now.getSeconds(),this);
         this.prevMs = performance.now();
         this.loop();
     }
@@ -224,7 +415,8 @@ class ClockApp {
     }
     render(ctx){
         ctx.clearRect(0,0,this.canvasWidth,this.canvasHeight);
-        ctx.fillStyle = "rgb(197, 188, 188)";
+        //ctx.fillStyle = "rgb(197, 188, 188)";
+        ctx.fillStyle = "rgb(255, 255, 255)";
         ctx.fillRect(0,0,this.canvasWidth,this.canvasHeight);
 
         this.clock.render(ctx);
